1aaab61941bd96dc0d73026df1b0cbbe0cde6061
Lehatran <leha.tran277@gmail.com>
2024-11-09 23:48:05 +0700

    User management: edit profile, change-password, register as landlord

--- Front-end/RoomSocialFE/src/app/app.module.ts
+++ Front-end/RoomSocialFE/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditProfileComponent } from './UserManagement/edit-profile/edit-profile.component';
+import { ChangePasswordComponent } from './UserManagement/change-password/change-password.component';
+import { RegisterLandlordComponent } from './UserManagement/register-landlord/register-landlord.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditProfileComponent,
+    ChangePasswordComponent,
+    RegisterLandlordComponent
   ],
   imports: [
     BrowserModule,
