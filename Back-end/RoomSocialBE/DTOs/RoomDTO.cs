57a590d5570562d69ee1fbb6d2b52181ff7fac0c
XuanAn19 <nguyenxuanan19082003@gmail.com>
2024-11-26 21:02:28 +0700

    add

--- Back-end/RoomSocialBE/DTOs/RoomDTO.cs
+++ Back-end/RoomSocialBE/DTOs/RoomDTO.cs
@@ -1,5 +1,5 @@
 ﻿using RoomSocialBE.Authentication;
-using RoomSocialBE.Model;
+using RoomSocialBE.Models;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
